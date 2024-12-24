47f2b131d22dbe0b9b132c76817a6ab486afd078
De vona C <devonacanada@gmail.com>
2024-12-23 22:48:39 -0500

    reactionSchema model added and coded

---
+++ social_network_api/reactionSchemaModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const reactionSchema = new Schema(
+  {
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp) => timestamp.toISOString(),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
